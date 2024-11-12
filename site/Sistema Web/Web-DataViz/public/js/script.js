ab741b64994d402058ae683634a3d7c76d91027b
GuiSantos04 <guilherme.fsantos@sptech.school>
2024-11-11 21:30:13 -0300

    Atualização dash

--- site/Sistema Web/Web-DataViz/public/js/script.js
+++ site/Sistema Web/Web-DataViz/public/js/script.js
@@ -7,14 +7,14 @@ function logar() {
   var mensagem = "";
 
   if (login == "Frizza" && senhaLogin == "Frizza@12345") {
-    estrutura_login.innerHTML = `<header>
+    estrutura_login.innerHTML = `
+    <header>
         <img src="./img/logo-png.png" class="logo">
     </header>
     <div class="container-login">
         <div class="card cadastro">
             <h1>Bem vindo ${login}!</h1>
             <a href="Dash-Teste.html"><img width="100px" src="./img/verificar.png"></a>
-
             <div class=cadastro_realizado>
                 <span>Clique no botão verde para ser redirecionado a sua página.</span>
             </div>
@@ -33,10 +33,10 @@ function cadastrar() {
   var empresa = ipt_empresa.value;
   var nome = ipt_nome.value;
   var repre = ipt_repre.value;
-  var cep = ipt_cep.value;
-  var num = ipt_num.value;
-  var cidade = ipt_cidade.value;
-  var estado = ipt_estado.value;
+  // var cep = ipt_cep.value;
+  // var num = ipt_num.value;
+  // var cidade = ipt_cidade.value;
+  // var estado = ipt_estado.value;
   var email = ipt_email.value;
   var cnpj = ipt_cnpj.value;
   var usuario = ipt_usuario.value;
@@ -70,17 +70,17 @@ function cadastrar() {
   var tam_nome = nome.length;
   var tam_cnpj = cnpj.length;
   var tam_repre = repre.length;
-  var tam_cep = cep.length;
-  var tam_num = num.length;
-  var tam_cidade = cidade.length;
-  var tam_estado = estado.length;
+  // var tam_cep = cep.length;
+  // var tam_num = num.length;
+  // var tam_cidade = cidade.length;
+  // var tam_estado = estado.length;
   var tam_email = email.length;
   var tam_usuario = usuario.length;
   var tam_confirmacao = comfirmacao.length;
 
   // VARIAVEL INTEIROS
-  var cep_int = parseInt(cep);
-  var num_int = parseInt(num);
+  // var cep_int = parseInt(cep);
+  // var num_int = parseInt(num);
   var cnpj_int = parseInt(cnpj);
 
   //VARIAVEIS USER VALIDAÇÃO
@@ -101,12 +101,12 @@ function cadastrar() {
 
   //VALIDAÇÂO TAMANHO
   if (
-    tam_num == 0 ||
+    // tam_num == 0 ||
     tam_email == 0 ||
     tam_empresa == 0 ||
-    tam_cep == 0 ||
-    tam_cidade == 0 ||
-    tam_estado == 0 ||
+    // tam_cep == 0 ||
+    // tam_cidade == 0 ||
+    // tam_estado == 0 ||
     tam_nome == 0 ||
     tam_usuario == 0 ||
     tamanho_senha == 0 ||
@@ -114,11 +114,13 @@ function cadastrar() {
     tam_repre == 0
   ) {
     alert("Todos os campos devem ser inseridos!");
-  } else if (tam_cep < 8 || cep_int != cep || cep < 0) {
-    alert("O CEP deve ser um número inteiro com 8 digítos!");
-  } else if (num_int != num || num < 0) {
-    alert("O número deve ser um valor inteiro!");
-  } else if (tam_cnpj < 14 || cnpj_int != cnpj || cnpj < 0) {
+  } else 
+  // if (tam_cep < 8 || cep_int != cep || cep < 0) {
+  //   alert("O CEP deve ser um número inteiro com 8 digítos!");
+  // } else if (num_int != num || num < 0) {
+  //   alert("O número deve ser um valor inteiro!");
+  // } else 
+  if (tam_cnpj < 14 || cnpj_int != cnpj || cnpj < 0) {
     alert("O CNPJ deve ser um número inteiro com 14 digítos!");
   } else if (
     arroba_email == false ||
@@ -135,7 +137,9 @@ function cadastrar() {
     especial_ecomercial_user ||
     user_sem_espaco != usuario
   ) {
-    alert("O nome de usuário não pode conter nenhum desses caracteres especiais (!, @, #, $, %, &) nem espaço!");
+    alert(
+      "O nome de usuário não pode conter nenhum desses caracteres especiais (!, @, #, $, %, &) nem espaço!"
+    );
   } else if (
     tamanho_senha >= 8 &&
     tamanho_senha <= 12 &&
@@ -159,26 +163,54 @@ function cadastrar() {
       nove_senha) &&
     senha == comfirmacao
   ) {
-    estrutura.innerHTML = `
-        <header>
-            <img src="./img/logo-png.png" class="logo">
-        </header>
-        <div class="container-login">
-            <div class="card cadastro">
-                <h1>Cadastro realizado com sucesso!</h1>
-                <a href="login.html"><img width="100px" src="./img/verificar.png"></a>
-      
-                <div class=cadastro_realizado>
-                    <span>Clique no botão verde para ser redirecionado a tela tela de login.</span>
-                </div>
-            </div>
-        </div>
-        <footer>
-            <span> &copy; 2024 DataTech Security </span>
-        </footer>`;
+    continuacao.innerHTML = `
+        <div class="campo">
+      <span>CEP:</span>
+      <input type="number" id="ipt_cep" placeholder="00000000" />
+    </div>
+    <div class="campo">
+      <span>Número:</span>
+      <input type="number" id="ipt_num" placeholder="Digite o número" />
+    </div>
+    <div class="campo">
+      <span>Cidade:</span>
+      <input
+        type="text"
+        id="ipt_cidade"
+        placeholder="Digite a cidade"
+      />
+    </div>
+    </div>
+    <div class="form-column">
+    <div class="campo">
+      <span>Estado:</span>
+      <input
+        type="text"
+        id="ipt_estado"
+        placeholder="Digite o Estado"
+      />
+    </div>
+    <button onclick="cadastrar1()" class="button">Seguinte</button>`;
   } else {
     alert(
       "A senha deve conter ao menos: \n- 1 letra Maiuscula \n- 1 letra minuscula \n- 1 número \n- 1 caractere especial (!, @, #, $, %, &); \n A confirmação deve ser igual a senha descrita no campo anterior!"
     );
   }
 }
+// estrutura.innerHTML = `
+//         <header>
+//             <img src="./img/logo-png.png" class="logo">
+//         </header>
+//         <div class="container-login">
+//             <div class="card cadastro">
+//                 <h1>Cadastro realizado com sucesso!</h1>
+//                 <a href="login.html"><img width="100px" src="./img/verificar.png"></a>
+
+//                 <div class=cadastro_realizado>
+//                     <span>Clique no botão verde para ser redirecionado a tela tela de login.</span>
+//                 </div>
+//             </div>
+//         </div>
+//         <footer>
+//             <span> &copy; 2024 DataTech Security </span>
+//         </footer>`;
