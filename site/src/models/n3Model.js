53cc6497101f20931e66a4001e6561f0c7cca798
silveriolaridev <larissa.silverio@sptech.school>
2024-11-30 20:29:56 -0300

    tirando redundancias

--- site/src/models/n3Model.js
+++ site/src/models/n3Model.js
@@ -1,32 +1,21 @@
 async function pergunta(mensagem, modeloIA) {
-  try {
-      var resultado = await modeloIA.generateContent(`Em um curto paragrafo responda: ${mensagem}`);
-      
-      if (resultado && resultado.response) {
-          
-          if (Array.isArray(resultado.response.candidates) && resultado.response.candidates.length > 0) {
-              const candidato = resultado.response.candidates[0];
-              
-              const respostaTexto = candidato.content?.parts?.[0]?.text;
-              
-              if (respostaTexto) {
-                  console.log("Resposta gerada:", respostaTexto);
-                  return respostaTexto; 
-              } else {
-                  throw new Error("Texto não encontrado no candidato");
-              }
-          } else {
-              throw new Error("Candidatos não encontrados ou não estão no formato esperado");
-          }
-      } else {
-          throw new Error("Resposta do modelo não encontrada ou mal formatada");
-      }
-  } catch (error) {
-      console.error("Erro ao obter resposta do modelo:", error);
-      throw error;
-  }
+    try {
+        var resultado = await modeloIA.generateContent(`Em um curto paragrafo responda: ${mensagem}`);
+
+        const candidato = resultado.response.candidates[0];
+        const respostaTexto = candidato.content?.parts?.[0]?.text;
+
+        if (respostaTexto) {
+            console.log("Resposta gerada:", respostaTexto);
+            return respostaTexto;
+        }
+
+    } catch (error) {
+        console.error("Erro ao obter resposta do modelo:", error);
+        throw error;
+    }
 }
 
 module.exports = {
-  pergunta
+    pergunta
 }
