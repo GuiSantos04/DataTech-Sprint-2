c7571d00f79c596d01db7d0510b9e2e759309233
vknaraujo <vitor.aoliveira@sptech.school>
2024-12-03 10:52:33 -0300

    alterações na mensagem de execução

--- site/app.js
+++ site/app.js
@@ -40,15 +40,17 @@ app.use("/n3", n3Router);
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    ####       ##     ######     ##     ######   ######    ####    ##  ##  
+    ## ##     ####      ##      ####      ##     ##       ##  ##   ##  ##  
+    ##  ##   ##  ##     ##     ##  ##     ##     ##       ##       ##  ##  
+    ##  ##   ######     ##     ######     ##     ####     ##       ######
+    ##  ##   ##  ##     ##     ##  ##     ##     ##       ##       ##  ##  
+    ## ##    ##  ##     ##     ##  ##     ##     ##       ##  ##   ##  ##  
+    ####     ##  ##     ##     ##  ##     ##     ######    ####    ##  ##  
+
+
     \n\n\n                                                                                                 
-    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
+    Servidor já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
